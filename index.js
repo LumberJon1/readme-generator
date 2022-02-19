f579e34e60c1394f9526d6c5a4b3c591f8326530
Jonathan W <jwilliams12117@gmail.com>
2022-02-18 21:18:07 -0700

    Added inquirer objects for each question a user would be prompted with

--- index.js
+++ index.js
@@ -1,14 +1,115 @@
 
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [];
+//Test data for running the prompts
+const testData = ["Project Name", "Project Description", "License Type", "Install by downloading Node and then running node index", "Use this to generate a professional README.", "Please don't contribute to this project.", "Test Instructions", "GitBasher2000", "gitbasher@hotmail.com"];
+
+//user input prompts
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the name of the project?",
+            validate: title => {
+                if (title) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter a project title.");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a brief description of the project.",
+            validate: description => {
+                if (description) {
+                    return true;
+                }
+                else {
+                    console.log("Please provide a project description.");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please provide installation instructions, if applicable.",
+            default: "No installation is required to run this project."
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "How should users interact with/use this program?",
+            validate: usage => {
+                if (usage) {
+                    return true;
+                }
+                else {
+                    console.log("Please provide some usage instructions for this program.");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "How should other developers help contribute to this project? (skip if you do not wish for other developers to contribute.)",
+            default: "Please do not contribute directly to this project.  If you have a suggestion, reach out to me directly."
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Enter any applicable test information.",
+            default: "No information on testing."
+        },
+        {
+            type: "",
+            name: "license",
+            message: "What kind of license was this created under?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",
+            validate: github => {
+                if (github) {
+                    return true;
+                }
+                else {
+                    console.log("Please provide a GitHub username.");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email address.",
+            validate: email => {
+                if (email) {
+                    return true;
+                }
+                else {
+                    console.log("Please provide a valid email address.");
+                }
+            }
+        }
+])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser();
+    const [...all] = testData; 
+}
 
 // Function call to initialize app
 init();
